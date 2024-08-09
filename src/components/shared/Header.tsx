03eb4566f4f7eeaccbf9deabacf54a340e6877ce
kimia <farshadniakimia@gmail.com>
2024-08-09 20:21:27 +0330

    add class body and header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
     return (
         <header>
             <DiscountNews />
-            <div className='px-5 md:px-0 shadow-lg bg-slate-100/50 sticky top-0 z-50 backdrop-filter backdrop-blur-[20px] w-full duration-500 h-20 flex items-center'>
+            <div className='px-5 md:px-0 shadow-lg bg-white sticky top-0 z-50 backdrop-filter backdrop-blur-[20px] w-full duration-500 h-20 flex items-center'>
                 <div className='container mx-auto flex items-center justify-center relative'>
                     <div className='absolute left-0 flex flex-shrink-0 items-center justify-center w-20 h-20'>
                         <img src="/images/logo.png" alt="logo" />
